b5908194985cdec108b7990e0410dc6abfd3a5ce
Vyacheslav Yugzov <Yugzov96@mail.ru>
2022-08-29 18:28:17 +0400

    Change Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,37 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(reversingByRule(reader.readLine()));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(new Reversing().reversingByRule(scanner.nextLine()));
     }
+}
 
-    public static String reversingByRule(String line) {
-        String[] words = line.split(" ");
+class Reversing {
+    private final String EMP = "";
+    private final String SEP = " ";
+    private final String PATTERN = "[^a-zA-Z]";
+
+    public String reversingByRule(String line) {
+        String[] words = line.split(SEP);
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < words.length; i++) {
-            StringBuilder builder = new StringBuilder(words[i].replaceAll("[^a-zA-Z]", "")).reverse();
+        for (String word : words) {
+            StringBuilder builder = new StringBuilder(word.replaceAll(PATTERN, EMP)).reverse();
 
-            for (int j = 0; j < words[i].length(); j++) {
-                if (!(Character.isLetter(words[i].charAt(j)))) {
-                    builder.insert(j, words[i].charAt(j));
+            for (int i = 0; i < word.length(); i++) {
+                if (!(Character.isLetter(word.charAt(i)))) {
+                    builder.insert(i, word.charAt(i));
                 }
             }
 
-            result.append(builder.toString()).append(" ");
+            result.append(builder).append(SEP);
         }
 
         return result.toString().trim();
     }
 }
-
