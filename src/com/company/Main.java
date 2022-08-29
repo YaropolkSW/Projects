89ed64abf5a84b175e77f457b88077fdfd50d0da
Vyacheslav Yugzov <Yugzov96@mail.ru>
2022-08-29 21:23:42 +0400

    Separate class Main and class Reversing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,13 @@
 package com.company;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(new Reversing().reversingByRule(scanner.nextLine()));
-    }
-}
-
-class Reversing {
-    private final String EMP = "";
-    private final String SEP = " ";
-    private final String PATTERN = "[^a-zA-Z]";
-
-    public String reversingByRule(String line) {
-        String[] words = line.split(SEP);
-        StringBuilder result = new StringBuilder();
-
-        for (String word : words) {
-            final StringBuilder builder = new StringBuilder(word.replaceAll(PATTERN, EMP)).reverse();
-
-            for (int i = 0; i < word.length(); i++) {
-                if (!(Character.isLetter(word.charAt(i)))) {
-                    builder.insert(i, word.charAt(i));
-                }
-            }
-
-            result.append(builder).append(SEP);
-        }
-
-        return result.toString().trim();
+        final Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String line = scanner.nextLine();
+        System.out.println("Ваш результат: " + new Reversing().reversingByRule(line));
     }
 }
